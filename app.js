a6859b0eeb11a2e5640b84986688657e3d90dbb3
kprocks <you@example.com>
2017-12-12 19:22:39 +0530

    remove some bugs ..

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
  const express = require('express');
  const app = express();
- const config = require('./config/config.js');
- app.use('view engine','ejs')
+ const config = require('./config/config');
+ const Routes = require('./controllers/routes');
+ app.set('view engine', 'ejs')
 
+ Routes(app);
 
  app.listen(config.dev.port, () => {
-   console.log('server started on :' + config.dev.port);
+     console.log('server started on :' + config.dev.port);
  });
--- controllers/routes.js
+++ controllers/routes.js
@@ -1,5 +1,5 @@
-module.exports = () => {
-  app.get('/', (req, res) = > {
+module.exports = (app) => {
+  app.get('/', (req, res) => {
     res.render('index');
   });
 
